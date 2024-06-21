b4edba4331638dfa7b5be292836e5dcda0eb1570
lumiere-on <mjw0214@ewhain.net>
2024-06-21 23:47:33 +0900

    mission8_johnny_src code

---
+++ src/main/java/umc/spring/apiPayload/exception/handler/FoodCategoryHandler.java
@@ -0,0 +1,10 @@
+package umc.spring.apiPayload.exception.handler;
+
+import umc.study.apiPayload.code.BaseErrorCode;
+import umc.study.apiPayload.exception.GeneralException;
+
+public class FoodCategoryHandler extends GeneralException {
+    public FoodCategoryHandler(BaseErrorCode errorCode) {
+        super(errorCode);
+    }
+}
