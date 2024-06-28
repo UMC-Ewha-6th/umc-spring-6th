129d95898610dbc2e886f2e496349c7d562db393
solsoleee <wsw3727@gmail.com>
2024-06-28 20:58:13 +0900

    missin10 제출

---
+++ src/main/java/umc/study/web/dto/MemberRequestDTO.java
@@ -0,0 +1,33 @@
+package umc.study.web.dto;
+
+import lombok.Getter;
+import umc.study.validation.annotation.ExistCategories;
+import jakarta.validation.Valid;
+import jakarta.validation.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.util.List;
+
+public class MemberRequestDTO {
+
+    @Getter
+    public static class JoinDto{
+        @NotBlank
+        String name;
+        @NotNull
+        Integer gender;
+        @NotNull
+        Integer birthYear;
+        @NotNull
+        Integer birthMonth;
+        @NotNull
+        Integer birthDay;
+        @Size(min = 5, max = 12)
+        String address;
+        @Size(min = 5, max = 12)
+        String specAddress;
+        @ExistCategories
+        List<Long> preferCategory;
+    }
+}
